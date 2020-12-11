3ddfac6cd8cd160c5a5bac760160e947ea74810d
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2020-12-11 23:22:14 +0900

    train_test_split stage added

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,15 +1,13 @@
-import yaml
-
 import kaggle
+
+from src.utils.utils import read_params
 
 
 def get_data(dataset, path):
-	kaggle.api.authenticate()
-	kaggle.api.dataset_download_files(dataset, path=path, unzip=True)
+    kaggle.api.authenticate()
+    kaggle.api.dataset_download_files(dataset, path=path, unzip=True)
 
 
-if __name__== '__main__':
-    with open("params.yaml", 'r') as fd:
-        params = yaml.safe_load(fd)
-
+if __name__ == '__main__':
+    params = read_params()
     get_data(params['kaggle_dataset'], 'data/raw')
---
+++ src/features/train_test_split.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import src.utils.utils as utils
+
+
+def custom_train_test_split(file_path, test_size, split_seed):
+    df = pd.read_csv(file_path)
+    train, test = train_test_split(df, test_size=test_size, random_state=split_seed)
+    return train, test
+
+
+if __name__ == '__main__':
+    params = utils.read_params()
+
+    train_df, test_df = custom_train_test_split(
+        params['file_path'],
+        params['test_size'],
+        params['split_seed']
+    )
+
+    train_df.to_csv("data/processed/train.csv", index=False)
+    test_df.to_csv("data/processed/test.csv", index=False)
