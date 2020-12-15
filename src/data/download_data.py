ad1930f3776b67a4d1699849fbc2c35e819565ff
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2020-12-16 01:04:02 +0900

    add type hints and docstrings to download_data.py and train_test_split.py

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,9 +1,21 @@
+from typing import NoReturn
+
 import kaggle
 
 from src.utils.read_params import read_params
 
 
-def get_data(dataset, path):
+def get_data(dataset: str, path: str) -> NoReturn:
+    """Authenticate and download Kaggle dataset.
+
+    Parameters
+    ----------
+    dataset : str
+        Kaggle dataset name in "username/dataset_name" format.
+    path : str
+        Directory path.
+
+    """
     kaggle.api.authenticate()
     kaggle.api.dataset_download_files(dataset, path=path, unzip=True)
 
--- src/features/train_test_split.py
+++ src/features/train_test_split.py
@@ -1,10 +1,31 @@
+from typing import Tuple
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from src.utils.read_params import read_params
 
 
-def custom_train_test_split(file_path, test_size, split_seed):
+def custom_train_test_split(file_path: str, test_size: float, split_seed: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Splitting data into train and test sets.
+
+    Parameters
+    ----------
+    file_path : str
+        Dataset path.
+    test_size : float
+        Proportion of the dataset to include in the train split.
+    split_seed : int
+        Seed for reproducible splitting.
+
+    Returns
+    -------
+    train : pd.DataFrame
+        Train set.
+    test : pd.DataFrame
+        Test set.
+
+    """
     df = pd.read_csv(file_path)
     train, test = train_test_split(df, test_size=test_size, random_state=split_seed)
     return train, test
