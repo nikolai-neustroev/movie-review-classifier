e5cc0465e33514cd4491639de54ffb8ea06d5bf3
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2020-12-16 22:12:19 +0900

    utils type hints and docstrings are added

--- src/utils/save_history.py
+++ src/utils/save_history.py
@@ -1,9 +1,23 @@
 import os
+from typing import NoReturn
 
 import pandas as pd
+import tensorflow as tf
 
 
-def save_history(history, timestamp, dir):
+def save_history(history: tf.keras.callbacks.History, timestamp: int, dir: str) -> NoReturn:
+    """Saves model fit history as JSON file.
+
+    Parameters
+    ----------
+    history : tf.keras.callbacks.History
+        Model fit history as History object.
+    timestamp : int
+        Timestamp used as model identifier.
+    dir : str
+        Directory where JSON file will be saved.
+
+    """
     hist_df = pd.DataFrame(history.history)
     # directory = 'data/fit_history/lstm_history'
     if not os.path.exists(dir):
